84a1b6afae082ef859b50ca88a8c3161e5f68652
Elopez230172 <elopez230172@gmail.com>
2026-09-14 10:23:17 -0600

    Agregar plantilla de configuración Firebase

---
+++ aulacontrol-firebase/js/firebase-config.js
@@ -0,0 +1,12 @@
+// Reemplaza estos valores con la configuración de tu aplicación Web de Firebase.
+// Esta configuración identifica el proyecto; la seguridad depende de Auth y de las reglas.
+export const firebaseConfig = {
+  apiKey: "REEMPLAZAR",
+  authDomain: "REEMPLAZAR.firebaseapp.com",
+  projectId: "REEMPLAZAR",
+  storageBucket: "REEMPLAZAR.firebasestorage.app",
+  messagingSenderId: "REEMPLAZAR",
+  appId: "REEMPLAZAR"
+};
+
+export const functionsRegion = "us-central1";
